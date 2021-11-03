827fe9f183dc250fc5391e81d8cad64ffa2b2a25
Harrilee <hl3794@nyu.edu>
2021-11-04 01:29:40 +0800

    updated database with auto incremented keys and more data fields

--- backend/models/sql/schema.sql
+++ backend/models/sql/schema.sql
@@ -9,7 +9,10 @@ CREATE TABLE User
     bio      VARCHAR(100),
     password VARCHAR(50),
     email    VARCHAR(50) UNIQUE,
-    avatar   VARCHAR(50),
+    avatar   VARCHAR(150),
+    gender   INT,
+    birthday DATE,
+    CHECK (gender in (0, 1, NULL)),
     PRIMARY KEY (user_id),
     INDEX (username)
 );
@@ -20,10 +23,10 @@ CREATE TABLE Post
     post_content         VARCHAR(100),
     post_author_id       INT,
     post_author_username VARCHAR(20),
-    post_status          NUMERIC(1, 0),
+    post_status          INT,
     CHECK (post_status in (0, 1, 2)),
-    post_product_price   NUMERIC(15, 0),
-    post_product_status  NUMERIC(1, 0),
+    post_product_price   NUMERIC(15, 2),
+    post_product_status  INT,
     CHECK (post_product_status in (0, 1)),
     PRIMARY KEY (post_id),
     FOREIGN KEY (post_author_id) REFERENCES User (user_id)
@@ -34,11 +37,12 @@ CREATE TABLE Post
 
 CREATE TABLE Image
 (
+    img_id         INT AUTO_INCREMENT,
     image_owner_id INT,
-    image_url      VARCHAR(100),
+    image_url      VARCHAR(150),
     image_alt_text VARCHAR(100),
     post_id        INT,
-    PRIMARY KEY (image_owner_id, image_url),
+    PRIMARY KEY (img_id),
     FOREIGN KEY (image_owner_id) REFERENCES User (user_id)
         ON DELETE CASCADE,
     FOREIGN KEY (post_id) REFERENCES Post (post_id)
@@ -47,12 +51,13 @@ CREATE TABLE Image
 
 CREATE TABLE Message
 (
+    msg_id      INT AUTO_INCREMENT,
     sender_id   INT,
     receiver_id INT,
     post_id     INT,
     send_time   TIMESTAMP,
     msg         VARCHAR(256),
-    PRIMARY KEY (sender_id, receiver_id, send_time, msg),
+    PRIMARY KEY (msg_id),
     FOREIGN KEY (post_id) REFERENCES Post (post_id)
         ON DELETE CASCADE,
     FOREIGN KEY (sender_id) REFERENCES User (user_id)
@@ -64,7 +69,7 @@ CREATE TABLE Message
 CREATE TABLE Address
 (
     name    VARCHAR(15),
-    phone   NUMERIC(15, 0),
+    phone   VARCHAR(20),
     address VARCHAR(50),
     user_id INT,
     PRIMARY KEY (address, user_id),
@@ -78,12 +83,12 @@ CREATE TABLE Deal
     post_id        INT,
     seller_id      INT,
     buyer_id       INT,
-    price          NUMERIC(15, 0),
+    price          NUMERIC(15, 2),
     sender_address VARCHAR(50),
     buyer_address  VARCHAR(50),
-    payment_status NUMERIC(1, 0),
+    payment_status INT,
     CHECK (payment_status in (0, 1)),
-    package_status NUMERIC(1, 0),
+    package_status INT,
     CHECK (package_status in (0, 1)),
     PRIMARY KEY (deal_id),
     FOREIGN KEY (seller_id) REFERENCES Address (user_id)
@@ -98,10 +103,11 @@ CREATE TABLE Deal
 
 CREATE TABLE Rate
 (
+    rate_id INT AUTO_INCREMENT,
     deal_id INT,
-    rate    NUMERIC(5, 0),
-    comment VARCHAR(100),
-    PRIMARY KEY (deal_id, rate, comment),
+    rate    INT,
+    comment VARCHAR(512),
+    PRIMARY KEY (rate_id),
     FOREIGN KEY (deal_id) REFERENCES Deal (deal_id)
         ON DELETE CASCADE
 );
@@ -110,6 +116,17 @@ CREATE TABLE Staff
 (
     staff_id   INT AUTO_INCREMENT,
     staff_name VARCHAR(20),
-    password   VARCHAR(15),
+    bio        VARCHAR(100),
+    password   VARCHAR(50),
+    email      VARCHAR(50) UNIQUE,
+    avatar     VARCHAR(150),
+    gender     INT,
+    birthday   DATE,
+    CHECK (gender in (0, 1, NULL)),
     PRIMARY KEY (staff_id)
 );
+
+CREATE TABLE Admin
+(
+    password VARCHAR(50)
+)
