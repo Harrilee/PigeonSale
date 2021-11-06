280bd1b5eeb55baca47f99c75a2b08eb719db557
robin <robin.rb.luo@gmail.com>
2021-11-07 00:01:44 +0800

    Fix auth link typo

--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -10,11 +10,12 @@ const getUserType = (link, usertype) => {
     else if (usertype === "admin") {
         link += "/admin";
     }
+    return link;
 }
 
 const login = (values, usertype) => {
     let AUTH_URL=API_URL+"/auth";
-    getUserType(AUTH_URL, usertype);
+    AUTH_URL = getUserType(AUTH_URL, usertype);
     AUTH_URL += "/login";
     return fetch(AUTH_URL, {
         mode: 'cors',
@@ -29,7 +30,7 @@ const login = (values, usertype) => {
 
 const signup = (values, usertype) => {
     let ACCOUNT_URL=API_URL+"/account";
-    getUserType(ACCOUNT_URL, usertype);
+    ACCOUNT_URL = getUserType(ACCOUNT_URL, usertype);
     // ADD NEW USER
     return fetch(ACCOUNT_URL, {
         mode: 'cors',
@@ -44,7 +45,7 @@ const signup = (values, usertype) => {
 
 const logout = (usertype) => {
     let AUTH_URL=API_URL+"/auth";
-    getUserType(AUTH_URL, usertype);
+    AUTH_URL = getUserType(AUTH_URL, usertype);
     AUTH_URL += "/logout";
     return fetch(AUTH_URL, {
         mode: 'cors',
