7057beda234ba0fa8293ea75028b6d21a6562f26
robin <robin.rb.luo@gmail.com>
2021-11-14 07:22:03 +0800

    Update layouts

--- frontend/src/layout/Navigation.js
+++ frontend/src/layout/Navigation.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import CreatePost from '../posts/CreatePost';
 
 function Navigation() {
-    
-    if (localStorage.isLoggedIn === "true" && localStorage.type === "user") {
+
+    if (localStorage.isLoggedIn === "true" && localStorage.usertype === "user"/*&& !window.location.href.includes("settings")*/) {
         return (
             <nav>
-                <NavLink to="/dashboard">Dashboard</NavLink>
-                <NavLink to="/cart">Cart</NavLink>
+                <CreatePost/>
+                <NavLink to={"/"+localStorage.username}>Profile</NavLink>
+                <NavLink to={"/"+localStorage.username+"/settings"}>Settings</NavLink>
                 <NavLink to="/logout">Log out</NavLink>
             </nav>
             
         )
     }
+    // else if (localStorage.isLoggedIn === "true" && window.location.href.includes("settings")) {
+    //     return (
+    //         <nav>
+    //             <NavLink to={"/"+localStorage.username}>Profile</NavLink>
+    //             <NavLink to={"/"+localStorage.username+"/settings"}>Settings</NavLink>
+    //             <NavLink to="/logout">Log out</NavLink>
+    //         </nav>
+    //     )
+    // }
     return (
        <nav>
             <NavLink to="/login">Log in</NavLink>
