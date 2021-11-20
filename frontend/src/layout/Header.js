687ff6fdbc18e18cdc67cb275d7743f1d5d41d55
robin <robin.rb.luo@gmail.com>
2021-11-21 06:16:40 +0800

    Update layout

--- frontend/src/layout/Header.js
+++ frontend/src/layout/Header.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navigation from './Navigation';
 import './Header.scss';
 import LogoCard from '../components/LogoCard';
+import { InputBase } from "@mui/material";
 
 function Header() {
+
+   const [keyword, setKeyword] = useState("");
+
+   const handleSearch = (e) => {
+      let keyword = e.target.value;
+      setKeyword(keyword);
+   }
+
+   const search = (e) => {
+      e.preventDefault();
+      window.location.href="/search/"+keyword;
+   }
+
     return (
        <header>
           <LogoCard title="Pigeon Sale" size="small" position="left" />
+          <form onSubmit={search}>
+          <InputBase id="search-bar"
+            placeholder="Search" 
+            onChange={handleSearch} 
+            value={keyword}
+         />
+         </form>
           <Navigation />
        </header>
     )
