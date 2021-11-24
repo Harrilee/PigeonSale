e9c87572d7aed4b0f91aa29f2c7a2dfe19eaab97
Harrilee <hl3794@nyu.edu>
2021-11-25 01:07:40 +0800

    updated image root, now login is required, changes are linked to database

--- backend/routes/image.py
+++ backend/routes/image.py
@@ -15,26 +15,29 @@ bp = Blueprint('image', __name__, url_prefix='/image')
 
 
 @bp.route('', methods=['POST'])
-# @check_login_user
+@check_login_user
 def receive_img():
     req = request.files
     if len(req) == 0:
         return api_fail("000", "No file found, getting 0 pictures")
-    paths = []  # the list of urls to return
+    urls = []  # the list of urls to return
     for image in req:  # pre-check if every image is "qualified"
         file = req[image]
         extension = file.filename.split('.')[-1].lower()
         if extension not in config.ALLOWED_IMAGE_EXTENSIONS:
             return api_fail("022", "File extension not accepted. Received: {}, Allowed: {}".format(
                 extension, str(config.ALLOWED_IMAGE_EXTENSIONS)))
+    # Store each image locally, and then sync the url to the database
     for image in req:
         file = req[image]
         filename = datetime.datetime.now().strftime("%Y%m%d%H%M%f") + '.' + file.filename.split('.')[-1].lower()
         path = os.path.join(config.IMAGE_STORAGE_PATH, filename)
         req[image].save(path)
-        paths.append(config.IMAGE_GET_PATH_BASE + filename)
-
-    return api_success({"urls": paths})
+        url = config.IMAGE_GET_PATH_BASE + filename
+        urls.append(url)
+        image_class = Image(image_owner_id=session['uid'], image_url=url)
+        image_class.insert_image_to_db()
+    return api_success({"urls": urls})
 
 
 @bp.route('/<filename>', methods=['GET'])
@@ -47,4 +50,4 @@ def get_pic(filename):
 
 @bp.route('/<filename>', methods=['DELETE'])
 def del_pic(filename):
-    pass
+    return api_fail("", "You cannot delete an image, you just can't!")
