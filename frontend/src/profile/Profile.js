cce8d52cdab1ee98ba80df8204594a05d03676dc
robin <robin.rb.luo@gmail.com>
2021-12-05 05:34:00 +0800

    Update public profile with ratings

--- frontend/src/profile/Profile.js
+++ frontend/src/profile/Profile.js
@@ -1,15 +1,52 @@
 import React, { useEffect, useState } from 'react';
-import { Box } from "@mui/material";
+import { Box, Tabs, Tab } from "@mui/material";
 import "./Profile.scss";
 import AccountService from "../services/account.service";
 import PublicPosts from '../posts/PublicPosts';
 import ProfileCard from './ProfileCard';
+import Ratings from './Ratings';
+
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`vertical-tabpanel-${index}`}
+        aria-labelledby={`vertical-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box sx={{ p: 3 }}>
+            {children}
+          </Box>
+        )}
+      </div>
+    );
+}
+
+function a11yProps(index) {
+    return {
+      id: `vertical-tab-${index}`,
+      'aria-controls': `vertical-tabpanel-${index}`,
+    };
+}
+
 
 function Profile({ match }) {
 
     const { params: { user_id } } = match;
     const [profile, setProfile] = useState("");
     const [loaded, setLoaded]= useState(false);
+
+    const [value, setValue] = useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
 
     const getProfile = () => {
         const request = {
@@ -30,6 +67,7 @@ function Profile({ match }) {
         });
     }
 
+
     useEffect(() => {
         if (profile.length === 0) {
             getProfile();
@@ -39,14 +77,28 @@ function Profile({ match }) {
     if (user_id !== localStorage.user_id) {
         return ( 
             <div id="profile-wrapper">
-                <Box id="profile-container">
-                    <ProfileCard data={profile} />
-                    <div id="public-posts">
-                        <h2 className="title center">Shop</h2>
+            <Box id="profile-container">
+                <ProfileCard data={profile} />
+                <Box id="tabs-container">
+                    <Tabs
+                        orientation="horizontal"
+                        value={value}
+                        onChange={handleChange}
+                        sx={{ borderBottom: 1, borderColor: 'divider' }}
+                        centered
+                    >
+                        <Tab label="Shop" {...a11yProps(0)} />
+                        <Tab label="Ratings" {...a11yProps(1)} />
+                    </Tabs>
+                    <TabPanel value={value} index={0}>
                         <PublicPosts isProfileLoaded={loaded} user_id={user_id} />
-                    </div>
+                    </TabPanel>
+                    <TabPanel value={value} index={1}>
+                        <Ratings user_id={user_id} isProfileLoaded={loaded} />
+                    </TabPanel>
                 </Box>
-            </div>
+            </Box>
+        </div>
         )
     }
     else {
